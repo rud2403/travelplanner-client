b01efaa3d4562342162d6f6dcc83e69f12003e65
bgm <qkrrudals2403@naver.com>
2024-06-01 18:08:31 +0900

    feat : ec2 server 통신 테스트

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { useState } from 'react';
+import axios from 'axios';
+
+export default function Signup() {
+    const [message, setMessage] = useState('Hello, click the button!');
+
+    const handleClick = async () => {
+        try {
+            const host = window.location.hostname === "localhost" ? 'http://localhost:8080/api/tests/Docker-test' : '/api/* http://15.165.4.45/:splat 200';
+
+            const response = await axios.get(host);
+            console.log('response : ', response);
+            setMessage(`Button clicked! Response: ${response.data}`);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            setMessage('Error fetching data');
+        };
+    }
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            Sign up Page
+            <p>{message}</p>
+            <button onClick={handleClick}>dd</button>
+        </main>
+    );
+
+}
