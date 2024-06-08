662357c6a00242af10a0a94a5222ced4a544f289
bgm <qkrrudals2403@naver.com>
2024-06-08 16:39:46 +0900

    feat : ec2 통신 테스트

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    /* config options here */
+    async rewrites() {
+        return [
+            {
+                source: '/:path*',
+                destination: 'http://15.165.4.45/:path*', // 백엔드 서버 IP로 변경
+            },
+        ];
+    },
+};
 
 export default nextConfig;
