f658ad67cc9caa4f1141ae20828b26393f2e1003
bgm <qkrrudals2403@naver.com>
2024-06-08 18:10:22 +0900

    refactor : 환경에 따른 엔드포인트 설정

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,14 @@
 const nextConfig = {
     reactStrictMode: true,
     async rewrites() {
+      // 환경에 따라 백엔드 서버 주소를 다르게 설정
+      const isDevelopment = process.env.NODE_ENV === 'development';
+      const backendUrl = isDevelopment ? 'http://localhost:8080' : 'http://15.165.4.45:8080';
+  
       return [
         {
           source: '/api/:path*',
-          destination: 'http://15.165.4.45:8080/api/:path*',
+          destination: `${backendUrl}/api/:path*`,
         },
       ];
     },
