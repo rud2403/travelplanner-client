d4ce658cefa3d78392e7e5a0db4eecefe290b4b6
bgm <qkrrudals2403@naver.com>
2024-06-23 01:40:05 +0900

    feat : zustand 전역상태관리 추가

---
+++ src/store/useTravelStore.ts
@@ -0,0 +1,44 @@
+import create from 'zustand';
+import { devtools } from 'zustand/middleware';
+
+interface TravelState {
+  departure: string;
+  arrival: string;
+  startDate: string;
+  endDate: string;
+  numberOfPeople: number;
+  budget: number;
+  setDeparture: (departure: string) => void;
+  setArrival: (arrival: string) => void;
+  setStartDate: (startDate: string) => void;
+  setEndDate: (endDate: string) => void;
+  setNumberOfPeople: (numberOfPeople: number) => void;
+  setBudget: (budget: number) => void;
+  resetState: () => void;
+}
+
+export const useTravelStore = create<TravelState>()(
+  devtools((set) => ({
+    departure: '',
+    arrival: '',
+    startDate: '',
+    endDate: '',
+    numberOfPeople: 1,
+    budget: 0,
+    setDeparture: (departure) => set({ departure }),
+    setArrival: (arrival) => set({ arrival }),
+    setStartDate: (startDate) => set({ startDate }),
+    setEndDate: (endDate) => set({ endDate }),
+    setNumberOfPeople: (numberOfPeople) => set({ numberOfPeople }),
+    setBudget: (budget) => set({ budget }),
+    resetState: () =>
+      set({
+        departure: '',
+        arrival: '',
+        startDate: '',
+        endDate: '',
+        numberOfPeople: 1,
+        budget: 0,
+      }),
+  }))
+);
