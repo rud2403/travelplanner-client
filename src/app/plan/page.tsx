bef8099a1957c1341187ecff34407b77bf4ce491
bgm <qkrrudals2403@naver.com>
2025-04-28 01:09:51 +0900

    refactor: Planning 컴포넌트 삭제 및 관련 코드 정리

--- src/app/plan/page.tsx
+++ src/app/plan/page.tsx
@@ -3,7 +3,7 @@
 import dynamic from 'next/dynamic';
 
 // 동적으로 로드하는 Planning
-const Planning = dynamic(() => import('@/components/plan/Planning'), {
+const Planning = dynamic(() => import('@/components/plan/tempname'), {
   loading: () => <div>Loading...</div>,
   ssr: false
 });
